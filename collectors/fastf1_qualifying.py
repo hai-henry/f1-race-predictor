8b66ff6b27bb8f6ae0510df54d8db131d439f37e
hai-henry <hvnryho@gmail.com>
2024-12-26 22:00:53 -0600

    Todo's added

--- collectors/fastf1_qualifying.py
+++ collectors/fastf1_qualifying.py
@@ -1,10 +1,19 @@
 import fastf1
 
+# TODO: Collect qualifying data for all races from 1950-2024
+# TODO: Actively add qualifying data to the dataset as collected like results
+
 
 def main():
     session = fastf1.get_session(2024, 1, "Q")
     session.load()
     print(session.results)
+    session.results.drop(
+        columns=["HeadshotUrl"], inplace=True
+    )  # Drop HeadshotUrl from session results
+    session.results.to_csv(
+        "data/raw/qualifying/fastf1_qualifying(2024).csv", index=False
+    )
 
 
 if __name__ == "__main__":
