434560239006dfad7f56f51508bbf293229f0e7d
hai-henry <hvnryho@gmail.com>
2025-01-26 20:22:30 -0600

    Results from early constructors not accurate

--- collectors/constructors.py
+++ collectors/constructors.py
@@ -1,3 +1,8 @@
+# The Constructors Championship was not awarded until 1958
+# This program will calculate the Constructors Championship standings from 1958 to 2024
+
+import os
+
 import pandas as pd
 
 DRIVER_STANDINGS_PATH = "data/raw/driver_standings(1950-2024).csv"
@@ -20,40 +25,43 @@ class Constructor:
 
 def main():
     driver_standings = pd.read_csv(DRIVER_STANDINGS_PATH)
-    driver_standings = driver_standings[driver_standings["season"] >= 2024]
+    driver_standings = driver_standings[driver_standings["season"] >= 1958]
 
-    # print(driver_standings)
+    for season, season_data in driver_standings.groupby("season"):
+        constructor_objects = {}
 
-    constructor_objects = {}
+        for _, driver in season_data.iterrows():
+            if driver["TeamName"] in constructor_objects:
+                constructor_objects[driver["TeamName"]].add_points(driver["Points"])
+            else:
+                constructor = Constructor(
+                    season=driver["season"],
+                    team=driver["TeamName"],
+                    drivers=[driver["FullName"]],
+                    points=driver["Points"],
+                )
+                constructor_objects[driver["TeamName"]] = constructor
 
-    for _, driver in driver_standings.iterrows():
-        if driver["TeamName"] in constructor_objects:
-            constructor_objects[driver["TeamName"]].add_points(driver["Points"])
-        else:
-            constructor = Constructor(
-                season=driver["season"],
-                team=driver["TeamName"],
-                drivers=[driver["FullName"]],
-                points=driver["Points"],
+        constructor_data = [
+            {
+                "season": constructor.season,
+                "Position": position + 1,
+                "TeamName": constructor.team,
+                "Points": constructor.points,
+            }
+            for position, constructor in enumerate(
+                sorted(
+                    constructor_objects.values(), key=lambda x: x.points, reverse=True
+                )
             )
-            constructor_objects[driver["TeamName"]] = constructor
+        ]
+        constructor_df = pd.DataFrame(constructor_data)
 
-    # Print constructor objects
-
-    constructor_data = [
-        {
-            "season": constructor.season,
-            "Position": position + 1,
-            "TeamName": constructor.team,
-            "Points": constructor.points,
-        }
-        for position, constructor in enumerate(
-            sorted(constructor_objects.values(), key=lambda x: x.points, reverse=True)
-        )
-    ]
-    constructor_df = pd.DataFrame(constructor_data)
-
-    print(constructor_df)
+        # Append to CSV file; write header only if file doesn't exist
+        if not os.path.exists(OUTPUT_PATH):
+            constructor_df.to_csv(OUTPUT_PATH, index=False, mode="w", header=True)
+        else:
+            constructor_df.to_csv(OUTPUT_PATH, index=False, mode="a", header=False)
 
 
 if __name__ == "__main__":
